8ae32c18464de1015d2da87a9ec33875d81de762
andy <1614593285@qq.com>
2019-09-11 16:10:06 +0800

    引进折线图

--- app/src/main/java/com/example/zxt/MainActivity.kt
+++ app/src/main/java/com/example/zxt/MainActivity.kt
@@ -1,12 +1,212 @@
 package com.example.zxt
 
+import android.graphics.Color
+import android.graphics.DashPathEffect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.Utils
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnChartValueSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // background color
+        chart.setBackgroundColor(Color.WHITE)
+        // disable description text
+        chart.getDescription().setEnabled(false)
+        // enable touch gestures
+        chart.setTouchEnabled(true)
+        // set listeners
+        chart.setOnChartValueSelectedListener(this)
+        chart.setDrawGridBackground(false)
+        // create marker to display box when values are selected
+        val mv = MyMarkerView(this, R.layout.custom_marker_view)
+
+        // Set the marker to the chart
+        mv.setChartView(chart)
+        chart.marker = mv
+
+        // enable scaling and dragging
+        chart.isDragEnabled = true
+        chart.setScaleEnabled(true)
+        // chart.setScaleXEnabled(true);
+        // chart.setScaleYEnabled(true);
+
+        // force pinch zoom along both axis
+        chart.setPinchZoom(true)
+
+        val xAxis: XAxis
+        run {
+            // // X-Axis Style // //
+            xAxis = chart.xAxis
+
+            // vertical grid lines
+            xAxis.enableGridDashedLine(10f, 10f, 0f)
+        }
+
+        val yAxis: YAxis
+        run {
+            // // Y-Axis Style // //
+            yAxis = chart.axisLeft
+
+            // disable dual axis (only use LEFT axis)
+            chart.axisRight.isEnabled = false
+
+            // horizontal grid lines
+            yAxis.enableGridDashedLine(10f, 10f, 0f)
+
+            // axis range
+            yAxis.axisMaximum = 200f
+            yAxis.axisMinimum = -50f
+        }
+
+
+        run {
+            // // Create Limit Lines // //
+            val llXAxis = LimitLine(9f, "Index 10")
+            llXAxis.lineWidth = 4f
+            llXAxis.enableDashedLine(10f, 10f, 0f)
+            llXAxis.labelPosition = LimitLine.LimitLabelPosition.RIGHT_BOTTOM
+            llXAxis.textSize = 10f
+//            llXAxis.typeface = tfRegular
+
+            val ll1 = LimitLine(150f, "Upper Limit")
+            ll1.lineWidth = 4f
+            ll1.enableDashedLine(10f, 10f, 0f)
+            ll1.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
+            ll1.textSize = 10f
+//            ll1.typeface = tfRegular
+
+            val ll2 = LimitLine(-30f, "Lower Limit")
+            ll2.lineWidth = 4f
+            ll2.enableDashedLine(10f, 10f, 0f)
+            ll2.labelPosition = LimitLine.LimitLabelPosition.RIGHT_BOTTOM
+            ll2.textSize = 10f
+//            ll2.typeface = tfRegular
+
+            // draw limit lines behind data instead of on top
+            yAxis.setDrawLimitLinesBehindData(true)
+            xAxis.setDrawLimitLinesBehindData(true)
+
+            // add limit lines
+            yAxis.addLimitLine(ll1)
+            yAxis.addLimitLine(ll2)
+            //xAxis.addLimitLine(llXAxis);
+        }
+
+        // add data
+//        seekBarX.setProgress(45)
+//        seekBarY.setProgress(180)
+        setData(45, 180f)
+
+        // draw points over time
+        chart.animateX(1500)
+
+        // get the legend (only possible after setting data)
+        val l = chart.legend
+
+        // draw legend entries as lines
+        l.form = Legend.LegendForm.LINE
+    }
+
+    private fun setData(count: Int, range: Float) {
+
+        val values = ArrayList<Entry>()
+
+        for (i in 0 until count) {
+
+            val `val` = (Math.random() * range).toFloat() - 30
+            values.add(Entry(i.toFloat(), `val`, resources.getDrawable(R.drawable.star)))
+        }
+
+        val set1: LineDataSet
+
+        if (chart.data != null && chart.data.dataSetCount > 0) {
+            set1 = chart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            set1.notifyDataSetChanged()
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "DataSet 1")
+
+            set1.setDrawIcons(false)
+
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f)
+
+            // black lines and points
+            set1.color = Color.BLACK
+            set1.setCircleColor(Color.BLACK)
+
+            // line thickness and point size
+            set1.lineWidth = 1f
+            set1.circleRadius = 3f
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false)
+
+            // customize legend entry
+            set1.formLineWidth = 1f
+            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set1.formSize = 15f
+
+            // text size of values
+            set1.valueTextSize = 9f
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f)
+
+            // set the filled area
+            set1.setDrawFilled(true)
+            set1.fillFormatter =
+                IFillFormatter { dataSet, dataProvider -> chart.axisLeft.axisMinimum }
+
+            // set color of filled area
+            if (Utils.getSDKInt() >= 18) {
+                // drawables only supported on api level 18 and above
+                val drawable = ContextCompat.getDrawable(this, R.drawable.fade_red)
+                set1.fillDrawable = drawable
+            } else {
+                set1.fillColor = Color.BLACK
+            }
+
+            val dataSets = ArrayList<ILineDataSet>()
+            dataSets.add(set1) // add the data sets
+
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+
+            // set data
+            chart.data = data
+        }
+    }
+
+
+    override fun onNothingSelected() {
+
+    }
+
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+
     }
 }
